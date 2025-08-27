fe99aab8caf4e71ac9e51f0f94e1a52f18a2070f
Arthur Ricardo Jacinto <arthur.jacinto01@gmail.com>
2025-08-27 16:58:54 -0300

    att1

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      <Text style={{color:"blue", fontSize:25}}>Arthur Ricardo Jacinto</Text>
+      <Text style={styles.titulo}> SENAI </Text>
+      <Image
+        source={{
+          url:"https://pm1.aminoapps.com/6410/4d28e79870b7246f5402ae8783dd0ec872400900_hq.jpg"
+        }}
+        ></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +24,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo:{
+    color: "red",
+    fontSize: 20,
+    fontWeight:"bold"
+  }
 });
